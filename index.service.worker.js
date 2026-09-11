8cd31553f890e38f8773cf623eefe51f54026971
Thomas Theilig <thomas.theilig@ktp-architekten.de>
2026-09-11 17:48:50 +0200

    Update Neon Math Run (2026-09-11 17:48)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789052145|54239886';
+const CACHE_VERSION = '1789141505|108316289';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon Math Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
